47cbc7193909e79be105d73a67290a61a3e4ba75
harshithramgr <harshithramgr@gmail.com>
2021-11-28 19:46:14 -0500

    Changes made for harshith

--- prime.py
+++ prime.py
@@ -1,4 +1,4 @@
-your_input=input("The_number_to_check:")
+your_input=input("The_number_to_check please:")
 for i in range(2,int(your_input)-1):
     if (int(your_input)%i==0):
         print ("not prime:(")
